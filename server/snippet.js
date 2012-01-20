51129ebdb2cfd0a730a7329dfabe062d09953f89
Roger Dudler <roger.dudler@gmail.com>
2012-01-20 17:31:51 +0100

    added title bar to snippet

--- server/snippet.js
+++ server/snippet.js
@@ -7,11 +7,13 @@ http.createServer(function (req, res) {
 	
 	var data = cache.get('snippet.js');
 	if (data) {
+		data = data.replace('<div class="highlight">', '<div class="highlight"><h3>Test</h3>');
 		res.end(data);
 	} else {
     	pygments.colorize('snippet.js', null, 'html', function(data) {
 			cache.put('snippet.js', data);
+			data = data.replace('<div class="highlight">', '<div class="highlight"><h3>Test</h3>');
         	res.end(data);
     	});
 	}
-}).listen(1337, "127.0.0.1");
+}).listen(1337, "10.221.0.184");
