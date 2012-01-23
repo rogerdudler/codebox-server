72e98de51dd8dff4b58ff90ae6071db08dfd608b
Roger Dudler <roger.dudler@gmail.com>
2012-01-23 13:21:02 +0100

    initial work

--- server/snippet.js
+++ server/snippet.js
@@ -1,19 +1,41 @@
 var http = require('http');
 var pygments = require('./pygments');
 var cache = require('./cache');
+var app = require('express').createServer();
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-	
-	var data = cache.get('snippet.js');
+app.get('/s/:id?', function(req, res) {
+	var id = req.params.id;
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	var file = 'snippet.js';
+	var title = file;
+	var data = cache.get(file);
 	if (data) {
-		data = data.replace('<div class="highlight">', '<div class="highlight"><h3>Test</h3>');
+		data = data.replace('<div class="highlight">', '<div class="highlight"><h3>' + title + '</h3>');
 		res.end(data);
 	} else {
-    	pygments.colorize('snippet.js', null, 'html', function(data) {
-			cache.put('snippet.js', data);
-			data = data.replace('<div class="highlight">', '<div class="highlight"><h3>Test</h3>');
+    	pygments.colorize(file, null, 'html', function(data) {
+			cache.put(file, data);
+			data = data.replace('<div class="highlight">', '<div class="highlight"><h3>' + title + '</h3>');
         	res.end(data);
     	});
 	}
-}).listen(1337, "10.221.0.184");
+	
+});
+
+function NotFound(msg){
+    this.name = 'NotFound';
+    Error.call(this, msg);
+    Error.captureStackTrace(this, arguments.callee);
+}
+
+NotFound.prototype.__proto__ = Error.prototype;
+
+app.get('/404', function(req, res){
+    throw new NotFound;
+});
+
+app.get('/500', function(req, res){
+    throw new Error('keyboard cat!');
+});
+
+app.listen(1337);
